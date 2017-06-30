3b8903a9b11ab164031862b1eb4d68e1a6daeb91
Frances Coronel <fvcproductions@gmail.com>
2017-06-30 14:14:09 -0700

    Welcome response

--- src/index.js
+++ src/index.js
@@ -4,42 +4,63 @@ var app = new alexa.app();
 /* app.launch provides an introductory message to our user. You can customize anything inside response.say to your own welcome message. */
 
 app.launch(function (request, response) {
-    response.say("Hello there, I am a bot created to help you find what to eat for lunch.");
+    response.say("Welcome to The 5 Love Languages Quiz. Originally crated by Gary Chapman.");
+    response.say("You're going to answer 30 questions to determine your primary love language.")
+    response.say("Knowing your love language is powerful, but knowing how they work in your relationships - that's a game changer.");
+    response.say("Let's get started!");
     response.shouldEndSession(false);
 })
 
 /* app.intent will be used by AWS Skills Portal to decipher our user’s intent (which is GetLunchSuggestions in this case) and possible things the user might say to ask Alexa for our intent. */
 
-app.intent('GetLunchSuggestions', {
+app.intent('Start Love Quiz', {
         "slots": {},
         "utterances": [
-            "what's for lunch",
-            "where should {I|we} go for lunch"
+            "what's my love language",
+            "how do I express my love",
+            "check my love language",
+            "figure out my love language",
+            "start the love language quiz",
+            "begin the love language quiz",
+            "love language quiz",
+            "quiz"
         ]
     },
     function (request, response) {
-        generate_suggestions(response);
+        generate_quiz(response);
         return;
     }
 );
 
+/* Questions from Love Language Quiz */
+
+var questions = [
+    {
+        "What is more meaningful to you?" :
+        [ 
+            "You receive a loving note/text/email for no special reason from your loved one.", // A
+            "Your partner and you hug." // E
+        ]
+    },
+    {
+        "What is more meaningful?" :
+        [
+            "You can spend alone time with your partner - just the two of you.", // B
+            "Your partner does something practical to help you out." // D
+        ]
+    }
+];
+
+var 
+
 /* generate_suggestions randomly picks a cuisine type under the variable food (which you can customize for your preferences) and outputs a suggestion. */
 
-function generate_suggestions(response) {
-    var food = ["Thai",
-        "Sushi",
-        "Chik-fil-a",
-        "Smash Burgers",
-        "Uncle Julio's"
-    ];
-
-    var rand = food[Math.floor(Math.random() * food.length)];
-
-
-    response.say("How about some " + rand + " today?");
+function generate_quiz(response) {
+    // get random question
+    var rand = questions[Math.floor(Math.random() * questions.length)]; 
+    // say random question
+    response.say(rand);
     response.send();
-
-
     return;
 }
 // Connect to lambda
