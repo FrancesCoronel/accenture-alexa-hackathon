d55ac4ab414ceb332d6f615782b46e89e4c79f46
Frances Coronel <fvcproductions@gmail.com>
2017-06-30 17:09:26 -0700

    Done for now!

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ var app = new alexa.app();
 /* app.launch provides an introductory message to our user. You can customize anything inside response.say to your own welcome message. */
 
 app.launch(function (request, response) {
-    response.say("Welcome to The 5 Love Languages Quiz. Originally crated by Gary Chapman.");
-    response.say("You're going to answer 30 questions to determine your primary love language.")
+    response.say("Welcome to The 5 Love Languages Quiz. Originally created by Gary Chapman.");
+    response.say("You're going to answer 30 questions to determine your primary love language.");
     response.say("Knowing your love language is powerful, but knowing how they work in your relationships - that's a game changer.");
     response.say("Let's get started!");
+    response.say("Our first question: What is more meaningful to you?");
+    response.say("Is it A. You receiving a loving note/text/email for no special reason from your loved one or B. You and your partner hugging.");
+    response.say("Okay, you chose A. That means you prefer words of affirmation over physical touch.");
+    response.say("Let's go on to the next questions!");
     response.shouldEndSession(false);
 })
 
 /* app.intent will be used by AWS Skills Portal to decipher our user’s intent (which is GetLunchSuggestions in this case) and possible things the user might say to ask Alexa for our intent. */
 
-app.intent('Start Love Quiz', {
+app.intent("StartQuiz", {
         "slots": {},
         "utterances": [
             "what's my love language",
@@ -22,50 +26,49 @@ app.intent('Start Love Quiz', {
             "figure out my love language",
             "start the love language quiz",
             "begin the love language quiz",
-            "love language quiz",
-            "quiz"
+            "start love language quiz",
+            "start quiz",
+            "what is my love language"
         ]
     },
     function (request, response) {
-        generate_quiz(response);
+        generateQuiz(response);
         return;
     }
 );
 
-/* Questions from Love Language Quiz */
-
-var questions = [
-    {
-        "What is more meaningful to you?" :
-        [ 
-            "You receive a loving note/text/email for no special reason from your loved one.", // A
-            "Your partner and you hug." // E
-        ]
-    },
-    {
-        "What is more meaningful?" :
-        [
-            "You can spend alone time with your partner - just the two of you.", // B
-            "Your partner does something practical to help you out." // D
-        ]
-    }
-];
-
-// A. Words of Affirmation
-// B. Quality Time
-// C. Receiving Gifts
-// D. Acts of Service
-// E. Physical Touch
-
-var a, b, c, d, e = 0;
-
 /* generate_suggestions randomly picks a cuisine type under the variable food (which you can customize for your preferences) and outputs a suggestion. */
 
-function generate_quiz(response) {
-    // get random question
-    var rand = questions[Math.floor(Math.random() * questions.length)]; 
-    // say random question
-    response.say(rand);
+function generateQuiz(response) {
+    /* Questions from Love Language Quiz */
+
+    // A. Words of Affirmation
+    // B. Quality Time
+    // C. Receiving Gifts
+    // D. Acts of Service
+    // E. Physical Touch
+
+    var a, b, c, d, e = 0;
+
+    var questions = [{
+            "What is more meaningful to you?": [
+                "You receive a loving note/text/email for no special reason from your loved one.", // A
+                "Your partner and you hug." // E
+            ]
+        },
+        {
+            "What is more meaningful?": [
+                "You can spend alone time with your partner - just the two of you.", // B
+                "Your partner does something practical to help you out." // D
+            ]
+        }
+    ];
+
+    response.say("Say A for the first choice or B for the second choice.");
+    response.say(questions[0]);
+    response.say("A is " + questions[0][0]);
+    response.say("B is " + questions[0][1]);
+    response.say("Okay, you chose A. So your primary love language is Words of Affirmation.");
     response.send();
     return;
 }
